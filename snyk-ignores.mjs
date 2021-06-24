c6a0a74a30715bb8b57d592fc48b5b0b5c7b997b
Iain Rose <iainrose@iainssnkbookpro.lan>
2021-06-24 13:38:15 -0700

    Output .snyk file to console

--- snyk-ignores.mjs
+++ snyk-ignores.mjs
@@ -8,36 +8,43 @@ const config = {
 const org = '86ecb770-4611-47bf-8caa-4a7ef9a1ea31'
 const project = 'accd6203-f06a-4c70-8c48-a6f7beb7f929'
 
-
-async function getIgnores(org, project) {
-    const response = await axios.get('/org/'+org+'/project/'+project+'/ignores', config)
-    return (response.data)
-}
-
-//const ignores = await getIgnores(org, project)
-const ignores = (await axios.get('/org/'+org+'/project/'+project+'/ignores', config)).data
+const ignores = (await axios.get(`/org/${org}/project/${project}/ignores`, config)).data
 
 const issues = Object.keys(ignores)
+console.log(issues)
+
+var snykIgnore = `# Snyk (https://snyk.io) policy file, patches or ignores known vulnerabilities.
+version: v1.14.0
+ignore:
+`
 
 issues.forEach(issue => {
     // Issue ID
     var issueID = issue
-    console.log(issueID)
 
     // Path
     var path = Object.keys(ignores[issue][0])
-    console.log(path[0])
+    if (path == '*') {
+        var pathString = `'*'`
+    } else
+    {
+        pathString = path
+    }
 
     // Reason
     var reason = ignores[issue][0][path].reason
     if(reason == '') {
         reason = 'None given'
     }
-    console.log(reason)
 
     // Expires
     var expires = ignores[issue][0][path].expires
-    console.log(expires)
 
-    //console.log(ignores[key][0][path])
+    snykIgnore += `  ${issueID}:
+    - ${pathString} :
+      reason: '${reason}'
+      expires: '${expires}'
+      `
 })
+
+console.log(snykIgnore);
